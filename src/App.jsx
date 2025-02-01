18811cdf14fd7240fc502885693195ffcb49050f
Abdulaziz <cursera.steam@gmail.com>
2025-02-01 15:58:18 +0500

    h2

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import React, { Component } from "react";
 export default class App extends Component {
   render() {
 
-   
 
-    return <div>App 
+
+    return <div>App
       <h1>Lorem ipsum dolor sit amet.</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti accusamus incidunt mollitia aut, et blanditiis rerum quaerat, voluptatibus asperiores doloribus dolore velit obcaecati corporis? Autem ullam modi, rerum distinctio ipsum laudantium fuga amet veniam magnam eius libero ad vero. Neque voluptatibus impedit vel omnis ad suscipit, necessitatibus nemo sit doloribus?</h1>
     </div>;
 
   }
