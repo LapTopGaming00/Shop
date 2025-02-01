aa3e935fda8bd8aec8df96603fd0191d11c20dcc
Shixnazar <a@a>
2025-02-01 15:51:01 +0500

    soz

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis excepturi, qui adipisci praesentium reiciendis similique dolore aliquid placeat quas modi?</h1>
       </div>
     )
   }
