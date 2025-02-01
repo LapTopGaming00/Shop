208dddb625b5b75ddf7f8ad3c667da8eb3e7a64c
Oktamov Shokhjahon <oktamovshokhjahon@gmail.com>
2025-02-01 15:55:27 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-
-   
-
-    return <div>App 
-      <h1>Lorem ipsum dolor sit amet.</h1>
-    </div>;
-
+    return (
+      <div>
+        App
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <h2>yana test</h2>
+      </div>
+    );
   }
 }
